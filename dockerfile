1d6d3f6ffb6c03d5df2236ed5016ba0d016a34fd
you tsai su <r8613266@gmail.com>
2022-03-01 11:44:04 +0800

    change src file to src directory

--- dockerfile
+++ dockerfile
@@ -1,4 +1,3 @@
-FROM mysql
 FROM node:14.16-alpine
 
 COPY ./ /usr/src/app
@@ -6,7 +5,6 @@ WORKDIR /usr/src/app
 
 RUN npm install
 
-USER 1001
 EXPOSE 3001
 
 CMD ["npm", "run", "dev"]
--- src/config.js
+++ src/config.js
@@ -1,13 +1,25 @@
 const moment = require('moment');
+require('dotenv').config();
+
+const {
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_DATABASE,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  JWT_SECRET_KEY,
+  REDIS_PORT,
+  REDIS_HOST,
+} = process.env;
 
 const JWT_CONFIG = {
-  JWT_SECRET_KEY: '123456',
+  JWT_SECRET_KEY: JWT_SECRET_KEY,
   EXPIRES_TIME: 60 * 1000
 };
 
 const REDIS_CONFIG = {
-  port: 6379,
-  host: '127.0.0.1'
+  port: REDIS_PORT,
+  host: REDIS_HOST
 };
 
 const REDIS_USER_EXPIRE_TIME = 3600;
@@ -15,11 +27,11 @@ const REDIS_USER_EXPIRE_TIME = 3600;
 const MYSQL_CONFIG = {
   client: 'mysql',
   connection: {
-    host : '127.0.0.1',
-    port: 3306,
-    user : 'root',
-    password : 'root',
-    database : 'book_store',
+    host : MYSQL_HOST,
+    port: MYSQL_PORT,
+    user : MYSQL_USER,
+    password : MYSQL_PASSWORD,
+    database : MYSQL_DATABASE,
     timezone: 'UTC',
     typeCast: function (field, next) {
       if (field.type == 'TIMESTAMP') {
